2d7d54acfaa86e8728ebf06970fec9e8a7b58ae8
jackpopp <jack.popp@hotmail.com>
2019-05-27 20:40:22 +0100

    service worker

---
+++ public/service-worker.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'lift-log';
+var urlsToCache = [
+  '/',
+  '/*.js',
+  '/*.css',
+  '/*.pmg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(e => console.error(e))
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      ).catch(e => console.error(e))
+    );
+  });
